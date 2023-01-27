24135d3e42db622b605308eeea4224d9c10d224a
Dovran <dseminov@gmail.com>
2023-01-27 10:27:13 +0300

    release

--- client/src/components/UI/Head/Head.jsx
+++ client/src/components/UI/Head/Head.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './head.css';
 
 export default function Head() {
   return (
-    <nav className="navbar bg-light border border-secondary mb-1 border-opacity-25" style={{ backgroundImage: 'url(https://t3.ftcdn.net/jpg/04/12/82/16/360_F_412821610_95RpjzPXCE2LiWGVShIUCGJSktkJQh6P.jpg)', backgroundSize: '100%' }}>
+    <nav className="navbar bg-light border border-secondary mb-1 border-opacity-25" id="font-link" style={{ backgroundImage: 'url(https://t3.ftcdn.net/jpg/04/12/82/16/360_F_412821610_95RpjzPXCE2LiWGVShIUCGJSktkJQh6P.jpg)', backgroundSize: '100%' }}>
       <div className="container-fluid">
         <Link to="/main" className="navbar-brand">Library</Link>
         <form className="d-flex" role="search">
